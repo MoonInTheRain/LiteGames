ff2f6559b313955484b3c7ee8456e74e532188b6
uesugi <wuesugi@hotmail.com>
2025-05-29 10:09:51 +0900

    [update] もろもろ更新

--- KoyoPuzzle/index.bb2c4.js
+++ KoyoPuzzle/index.bb2c4.js
@@ -1,4 +1,4 @@
-System.register(["./application.2f7fb.js"], function (_export, _context) {
+System.register(["./application.27d33.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
