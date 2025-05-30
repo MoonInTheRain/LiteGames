fc983a645850ceb18706d985f4ac54b0f7bbf90c
uesugi <wuesugi@hotmail.com>
2025-05-31 08:26:42 +0900

    [update] 画像追加

--- KoyoPuzzle/application.d6c55.js
+++ KoyoPuzzle/application.d6c55.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.97559.json';
+          this.settingsPath = 'src/settings.6063a.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
